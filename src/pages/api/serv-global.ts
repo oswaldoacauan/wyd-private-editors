f6b126a2d247ded80a80f2f018d8aa3ba6360a79
Oswaldo Acauan <oswaldoacauan@users.noreply.github.com>
2023-10-09 18:41:23 +0200

    Update serv-global.ts

--- src/pages/api/serv-global.ts
+++ src/pages/api/serv-global.ts
@@ -3,6 +3,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method) {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(" 1  1  1  1  1  1  -1  -1  -1  -1 ");
+    res.end(" 333  333  333  333  333  333  -1  -1  -1  -1 ");
   }
 }
